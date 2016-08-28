529498b13928c8766e3537093b71d38fd146b3d2
Ahmed Agabani <agabani@live.com>
2016-08-28 19:48:41 +0100

    Note and octave parser

---
+++ GuildWars2Orchestra.Tests.Unit/NoteParserTest.cs
@@ -0,0 +1,58 @@
+﻿using GuildWars2Orchestra.Parsers;
+using GuildWars2Orchestra.Values;
+using NUnit.Framework;
+
+namespace GuildWars2Orchestra.Tests.Unit
+{
+    public class NoteParserTest
+    {
+        [Test]
+        [TestCase("C,", Note.Keys.Note1, Note.Octaves.Low, 1, 1)]
+        [TestCase("D,", Note.Keys.Note2, Note.Octaves.Low, 1, 1)]
+        [TestCase("E,", Note.Keys.Note3, Note.Octaves.Low, 1, 1)]
+        [TestCase("F,", Note.Keys.Note4, Note.Octaves.Low, 1, 1)]
+        [TestCase("G,", Note.Keys.Note5, Note.Octaves.Low, 1, 1)]
+        [TestCase("A,", Note.Keys.Note6, Note.Octaves.Low, 1, 1)]
+        [TestCase("B,", Note.Keys.Note7, Note.Octaves.Low, 1, 1)]
+
+        [TestCase("C", Note.Keys.Note1, Note.Octaves.Middle, 1, 1)]
+        [TestCase("D", Note.Keys.Note2, Note.Octaves.Middle, 1, 1)]
+        [TestCase("E", Note.Keys.Note3, Note.Octaves.Middle, 1, 1)]
+        [TestCase("F", Note.Keys.Note4, Note.Octaves.Middle, 1, 1)]
+        [TestCase("G", Note.Keys.Note5, Note.Octaves.Middle, 1, 1)]
+        [TestCase("A", Note.Keys.Note6, Note.Octaves.Middle, 1, 1)]
+        [TestCase("B", Note.Keys.Note7, Note.Octaves.Middle, 1, 1)]
+
+        [TestCase("c", Note.Keys.Note1, Note.Octaves.High, 1, 1)]
+        [TestCase("d", Note.Keys.Note2, Note.Octaves.High, 1, 1)]
+        [TestCase("e", Note.Keys.Note3, Note.Octaves.High, 1, 1)]
+        [TestCase("f", Note.Keys.Note4, Note.Octaves.High, 1, 1)]
+        [TestCase("g", Note.Keys.Note5, Note.Octaves.High, 1, 1)]
+        [TestCase("a", Note.Keys.Note6, Note.Octaves.High, 1, 1)]
+        [TestCase("b", Note.Keys.Note7, Note.Octaves.High, 1, 1)]
+        [TestCase("c'", Note.Keys.Note8, Note.Octaves.High, 1, 1)]
+        public void it_parses_note(string text, Note.Keys key, Note.Octaves octave, int nominator, int denominator)
+        {
+            var noteParser = new NoteParser();
+
+            var note = noteParser.Parse(text);
+
+            Assert.That(note.Key, Is.EqualTo(key));
+            Assert.That(note.Octave, Is.EqualTo(octave));
+            Assert.That(note.Nominator, Is.EqualTo(nominator));
+            Assert.That(note.Denominator, Is.EqualTo(denominator));
+        }
+    }
+}
+
+/* ==== NOTE MAPPING ====
+
+    C,	D,	E,	F,	G,	A,	B,
+    ▼1	▼2	▼3	▼4	▼5	▼6	▼7
+
+    C	D	E	F	G	A	B
+    1	2	3	4	5	6	7
+
+    c	d	e	f	g	a	b	c'
+    ▲1	▲2	▲3	▲4	▲5	▲6	▲7	▲8
+*/
---
+++ GuildWars2Orchestra/Parsers/NoteParser.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using GuildWars2Orchestra.Values;
+
+namespace GuildWars2Orchestra.Parsers
+{
+    public class NoteParser
+    {
+        public Note Parse(string text)
+        {
+            var key = ParseKey(text);
+            var octave = ParseOctave(text);
+            return new Note(key, octave, 1, 1);
+        }
+
+        private static Note.Keys ParseKey(string text)
+        {
+            Note.Keys key;
+            switch (text)
+            {
+                case "C,":
+                case "C":
+                case "c":
+                    key = Note.Keys.Note1;
+                    break;
+                case "D,":
+                case "D":
+                case "d":
+                    key = Note.Keys.Note2;
+                    break;
+                case "E,":
+                case "E":
+                case "e":
+                    key = Note.Keys.Note3;
+                    break;
+                case "F,":
+                case "F":
+                case "f":
+                    key = Note.Keys.Note4;
+                    break;
+                case "G,":
+                case "G":
+                case "g":
+                    key = Note.Keys.Note5;
+                    break;
+                case "A,":
+                case "A":
+                case "a":
+                    key = Note.Keys.Note6;
+                    break;
+                case "B,":
+                case "B":
+                case "b":
+                    key = Note.Keys.Note7;
+                    break;
+                case "c'":
+                    key = Note.Keys.Note8;
+                    break;
+                default:
+                    throw new NotSupportedException(text);
+            }
+            return key;
+        }
+
+        private static Note.Octaves ParseOctave(string text)
+        {
+            Note.Octaves octave;
+            switch (text)
+            {
+                case "C,":
+                case "D,":
+                case "E,":
+                case "F,":
+                case "G,":
+                case "A,":
+                case "B,":
+                    octave = Note.Octaves.Low;
+                    break;
+                case "C":
+                case "D":
+                case "E":
+                case "F":
+                case "G":
+                case "A":
+                case "B":
+                    octave = Note.Octaves.Middle;
+                    break;
+                case "c":
+                case "d":
+                case "e":
+                case "f":
+                case "g":
+                case "a":
+                case "b":
+                case "c'":
+                    octave = Note.Octaves.High;
+                    break;
+                default:
+                    throw new NotSupportedException(text);
+            }
+            return octave;
+        }
+    }
+}
