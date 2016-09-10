b9281d1d95072665f5c33ff0eef8ed1420c29f96
Ahmed Agabani <agabani@live.com>
2016-09-10 01:02:38 +0100

    Updated options

--- GuildWars2Orchestra/ApplicationOptions.cs
+++ GuildWars2Orchestra/ApplicationOptions.cs
@@ -4,10 +4,15 @@ namespace GuildWars2Orchestra
 {
     internal class ApplicationOptions
     {
-        [Option('e', "emulated", Default = false, HelpText = "Play music using an emulated music player.")]
+        [Option('e', "emulated",
+            Default = false,
+            HelpText = "Play music using an emulated music player.",
+            Required = false)]
         public bool Emulated { get; set; }
 
-        [Option(Default = @"TestData\Guilty Crown - My Dearest.xml")]
+        [Value(0,
+            Default = @"TestData\Guilty Crown - My Dearest.xml",
+            HelpText = @"Location of .xml file or .mid file to be played.")]
         public string File { get; set; }
     }
 }
